ea7e15d6e84f319bd2ca2c507d21eb22049be2e2
luizrcs <luizrodrigo40@gmail.com>
2021-07-07 20:26:53 -0300

    First working version

--- serialization/src/main/kotlin/serialization/NbtEncoder.kt
+++ serialization/src/main/kotlin/serialization/NbtEncoder.kt
@@ -8,13 +8,25 @@ package br.com.luizrcs.nbt.serialization
 import br.com.luizrcs.nbt.core.tag.*
 import kotlinx.serialization.*
 import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.descriptors.StructureKind.*
 import kotlinx.serialization.internal.*
 import kotlinx.serialization.modules.*
 
-open class NbtEncoder : NamedValueEncoder() {
+fun <T> encodeToNbt(serializer: SerializationStrategy<T>, value: T): TagAny {
+	lateinit var tag: TagAny
+	val encoder = NbtEncoder { tag = it }
+	encoder.encodeSerializableValue(serializer, value)
+	return tag
+}
+
+inline fun <reified T> encodeToNbt(value: T) = encodeToNbt(serializer(), value)
+
+open class NbtEncoder(private val tagConsumer: (TagAny) -> Unit) : NamedValueEncoder() {
 	private val map = mutableMapOf<String, TagAny>()
 	
 	override val serializersModule = EmptySerializersModule
+	
+	open fun getTag(): TagAny = TagCompound(map)
 	
 	open fun putTag(key: String, tag: TagAny) {
 		map[key] = tag
@@ -28,26 +40,26 @@ open class NbtEncoder : NamedValueEncoder() {
 	override fun encodeTaggedDouble(tag: String, value: Double) = putTag(tag, TagDouble(value, tag))
 	override fun encodeTaggedString(tag: String, value: String) = putTag(tag, TagString(value, tag))
 	
-	override fun beginStructure(descriptor: SerialDescriptor) =
-		when (descriptor.kind) {
-			StructureKind.LIST -> ListNbtEncoder()
-			else               -> this
+	override fun beginStructure(descriptor: SerialDescriptor): NbtEncoder {
+		val consumer =
+			if (currentTagOrNull == null) tagConsumer
+			else { tag -> putTag(popTag(), tag) }
+		
+		val encoder = when (descriptor.kind) {
+			CLASS -> NbtEncoder(consumer)
+			LIST  -> ListNbtEncoder(consumer)
+			else  -> this
 		}
-	
-	open fun getTag(): TagAny = TagCompound(map)
-}
-
-class PrimitiveNbtEncoder : NbtEncoder() {
-	private lateinit var tagAny: TagAny
-	
-	override fun putTag(key: String, tag: TagAny) {
-		tagAny = tag
+		
+		return encoder
 	}
 	
-	override fun getTag() = tagAny
+	override fun endEncode(descriptor: SerialDescriptor) {
+		tagConsumer(getTag())
+	}
 }
 
-class ListNbtEncoder : NbtEncoder() {
+class ListNbtEncoder(tagConsumer: (TagAny) -> Unit) : NbtEncoder(tagConsumer) {
 	private val list = mutableListOf<TagAny>()
 	
 	override fun elementName(descriptor: SerialDescriptor, index: Int) = index.toString()
@@ -56,13 +68,5 @@ class ListNbtEncoder : NbtEncoder() {
 		list.add(key.toInt(), tag)
 	}
 	
-	override fun getTag() = TagList(TagType.TAG_BYTE, list)
+	override fun getTag() = TagList(list.first().type, list)
 }
-
-fun <T> encodeToNbt(serializer: SerializationStrategy<T>, value: T): TagAny {
-	val encoder = NbtEncoder()
-	encoder.encodeSerializableValue(serializer, value)
-	return encoder.getTag()
-}
-
-inline fun <reified T> encodeToNbt(value: T) = encodeToNbt(serializer(), value)
--- serialization/src/test/kotlin/serialization/Main.kt
+++ serialization/src/test/kotlin/serialization/Main.kt
@@ -3,8 +3,22 @@ package br.com.luizrcs.nbt.serialization
 import kotlinx.serialization.*
 
 @Serializable
-data class Test(val id: Int, val name: String, val data: ByteArray)
+data class TestA(val testCompound: TestB, val testList: List<TestC>, val timestamp: Long)
+
+@Serializable
+data class TestB(val fibonacciWithoutZero: IntArray)
+
+@Serializable
+data class TestC(val firstString: String, val secondString: String, val justAnInteger: Int)
 
 fun main() {
-	println(encodeToNbt(Test(0, "nbt", byteArrayOf(0, 1, 2))))
+	println(
+		encodeToNbt(
+			TestA(
+				TestB(intArrayOf(1, 1, 2, 3, 5, 8, 13, 21)),
+				listOf(TestC("I'm the first String :)", "I'm the second String, but order is not guaranteed :/", 1)),
+				System.currentTimeMillis()
+			)
+		)
+	)
 }
